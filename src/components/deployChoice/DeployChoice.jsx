92c82ba99c10751b10358a17a7bebddc1e563323
bautista gonzalez lazo <bautistagonzalezlazo@gmail.com>
2024-08-01 17:46:01 -0300

    Componente akash en landing

--- src/components/deployChoice/DeployChoice.jsx
+++ src/components/deployChoice/DeployChoice.jsx
@@ -1,67 +1,93 @@
 import React, { useEffect, useState } from 'react';
-import DeployOption from './DeployOption';
-import axios from 'axios';
+import DeployOption from "./DeployOption";
+import DeployOption2 from "./DeployOption2";
 
 const DeployChoice = () => {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [nodes, setNodes] = useState(null);
+  const [fluxData, setFluxData] = useState(null);
+  const [fluxNodes, setFluxNodes] = useState(null);
+  const [akashData, setAkashData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchFlux = async () => {
-      setLoading(true);
-      await fetch('https://stats.runonflux.io/fluxinfo?projection=benchmark')
-        .then((response) => response.json())
-        .then((data) => {
-          let totalSsd = 0,
-            totalRam = 0,
-            totalStorage = 0;
+      try {
+        const response = await fetch(
+          "https://stats.runonflux.io/fluxinfo?projection=benchmark"
+        );
+        const data = await response.json();
+        let totalSsd = 0,
+          totalRam = 0,
+          totalStorage = 0;
 
-          data.data.forEach((item) => {
-            const bench = item.benchmark.bench;
-            totalSsd += bench.ssd;
-            totalRam += bench.ram;
-            totalStorage += bench.cores;
-          });
-
-          // Convert MB to TB
-          totalSsd = totalSsd / 1024;
-          totalRam = totalRam / 1024;
-
-          setData({
-            totalSsd,
-            totalRam,
-            totalStorage,
-          });
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-          setLoading(false);
+        data.data.forEach((item) => {
+          const bench = item.benchmark.bench;
+          totalSsd += bench.ssd;
+          totalRam += bench.ram;
+          totalStorage += bench.cores;
         });
+
+        // Convert MB to TB
+        totalSsd = totalSsd / 1024;
+        totalRam = totalRam / 1024;
+
+        setFluxData({
+          totalSsd,
+          totalRam,
+          totalStorage,
+        });
+      } catch (error) {
+        console.error("Error fetching Flux data:", error);
+      }
     };
 
-    const fetchNodes = async () => {
-      setLoading(true);
-      await fetch('https://api.runonflux.io/daemon/getfluxnodecount')
-        .then((response) => response.json())
-        .then((data) => {
-          setNodes({
-            totalNodes: data.data.total,
-          });
-          setLoading(false);
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-          setLoading(false);
+    const fetchFluxNodes = async () => {
+      try {
+        const response = await fetch(
+          "https://api.runonflux.io/daemon/getfluxnodecount"
+        );
+        const data = await response.json();
+        setFluxNodes({
+          totalNodes: data.data.total,
         });
+      } catch (error) {
+        console.error("Error fetching Flux nodes:", error);
+      }
     };
 
-    fetchNodes();
-    fetchFlux();
+    const fetchAkash = async () => {
+      try {
+        const response = await fetch(
+          "https://api.cloudmos.io/v1/dashboard-data"
+        );
+        const data = await response.json();
+        const akashInfo = data.now;
+
+        const totalSsd = akashInfo.activeStorage / 1024 ** 4; // Convert bytes to TB
+        const totalRam = akashInfo.activeMemory / 1024 ** 4; // Convert bytes to TB
+        const totalStorage = akashInfo.activeCPU; // Assuming this is the total cores
+        const totalNodes = akashInfo.activeGPU;
+
+        setAkashData({
+          totalSsd,
+          totalRam,
+          totalStorage,
+          totalNodes,
+        });
+      } catch (error) {
+        console.error("Error fetching Akash data:", error);
+      }
+    };
+
+    const fetchAllData = async () => {
+      setLoading(true);
+      await Promise.all([fetchFlux(), fetchFluxNodes(), fetchAkash()]);
+      setLoading(false);
+    };
+
+    fetchAllData();
   }, []);
 
-  if (loading || !data || !nodes) {
+  if (loading || !fluxData || !fluxNodes || !akashData) {
     return <div>Loading...</div>; // Muestra un indicador de carga mientras se obtienen los datos
   }
 
@@ -73,8 +99,15 @@ const DeployChoice = () => {
         image="/fluxLanding.svg"
         title="The largest decentralized computing network"
         text="Discover the freedom of managing a cloud without the need of expertise or DevOps. Even if you're unfamiliar with new decentralized technologies, we make hosting stress-free and accessible for everyone, offering a straightforward and dependable experience in the realm of decentralization."
-        data={data}
-        nodes={nodes}
+        data={fluxData}
+        nodes={fluxNodes}
+      />
+      <DeployOption2
+        image="/akashLanding.svg" // Replace with actual image path
+        title="Akash Network"
+        text="Explore the power of Akash Network for your decentralized cloud needs. Akash offers a robust and flexible solution for all your hosting requirements, ensuring reliability and ease of use."
+        data={akashData}
+        nodes={akashData.totalNodes} // Assuming activeLeaseCount represents the number of active nodes
       />
       <button className="button-landing-4">DEPLOY NOW</button>
     </div>
---
+++ src/components/deployChoice/DeployOption2.jsx
@@ -0,0 +1,75 @@
+import Image from "next/image";
+import React from "react";
+import CountUp from "react-countup";
+
+const DeployOption2 = ({ image, title, text, className, data, nodes }) => {
+  return (
+    <div className={`deploy-option ${className}`}>
+      <div>
+        <Image alt="" src={image} width={250} height={150} />
+        <h2>{title}</h2>
+        <span>{text}</span>
+      </div>
+      <div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div className="deploy-numbers">
+            <div className="deploy-data">
+              <h4>Total Nodes</h4>
+              <p className="countup-large">
+                <CountUp
+                  start={0}
+                  end={nodes}
+                  duration={2.5}
+                  style={{ fontSize: "26px" }}
+                />
+              </p>
+            </div>
+
+            <div className="deploy-data">
+              <h4>Total RAM</h4>
+              <p className="countup-large">
+                <CountUp
+                  style={{ fontSize: "26px" }}
+                  start={0}
+                  end={data.totalRam}
+                  duration={2.5}
+                />{" "}
+                TB
+              </p>
+            </div>
+          </div>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <div className="deploy-data">
+            <h4>Total CPU</h4>
+            <p className="countup-large">
+              <CountUp
+                style={{ fontSize: "26px" }}
+                start={0}
+                end={data.totalStorage}
+                duration={2.5}
+              />
+            </p>
+          </div>
+          <div className="deploy-data">
+            <h4>Total SSD</h4>
+            <p className="countup-large">
+              <CountUp
+                style={{ fontSize: "26px" }}
+                start={0}
+                end={data.totalSsd}
+                duration={2.5}
+                decimal=","
+                decimals={2}
+              />{" "}
+              TB
+            </p>
+          </div>
+        </div>
+        <h5>Countries : 78</h5>
+      </div>
+    </div>
+  );
+};
+
+export default DeployOption2;
