e52212ebaea9978cb1782ee87632cf0d3706db6e
FOLTITA\Bauti <bautistagonzalezlazo@gmail.com>
2025-03-05 11:32:08 -0300

    Issues 05/03

---
+++ src/components/DeleteModal.jsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+const DeleteModal = ({ onClick, darkMode, name, onYes, id }) => {
+  const handleSave = () => {};
+
+  return (
+    <div className={`card3 ${darkMode ? "dark" : "light"}`}>
+      <Image
+        onClick={() => {
+          onClick();
+        }}
+        alt=""
+        src="/close.png"
+        width={20}
+        height={20}
+      />
+      <h3>You want to delete {name} deploy? </h3>
+      <span></span>
+      <div className="botonera-pay-modal">
+        <button onClick={()=>{onYes(id)}} >Yes</button>
+        <button>No</button>
+      </div>
+    </div>
+  );
+};
+
+export default DeleteModal;
