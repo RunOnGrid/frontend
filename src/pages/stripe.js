826bc118ae4b07c3d9a00c49f9e9832fbb26b64d
Hariseldon <benjamin@ongrid.run>
2025-06-04 11:29:41 -1000

    Pk stripe dev
    change to dev env

--- src/pages/stripe.js
+++ src/pages/stripe.js
@@ -6,7 +6,7 @@ import {
 } from "@stripe/react-stripe-js";
 
 const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY_DEV
 );
 
 export default function App() {
